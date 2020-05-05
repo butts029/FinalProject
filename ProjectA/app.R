423763ec7e0760b15fa38b7be216b39067dd29aa
Jessica Butts <butts029@umn.edu>
2020-05-05 17:14:13 -0400

    completed app for project A; added to project B - still need to check final pull one more time; added intro, analysis, and visualization to project C

---
+++ ProjectA/app.R
@@ -0,0 +1,171 @@
+# Link to active app: https://butts029.shinyapps.io/ProjectA/
+
+# Libraries
+library(shiny)         # For creating app
+library(tidyverse)     # For general manipulation
+library(rtweet)        # For pulling tweets
+library(RWeka)         # For tokenizing
+library(tm)            # For creating corpus
+library(qdap)          # For processing text
+library(textstem)      # For lemmatization
+library(wordcloud)     # For creating word cloud
+
+# Define UI for application
+ui <- fluidPage(
+
+    titlePanel("Twitter and COVID 19"),
+
+    # Sidebar with a select input for hashtag selection
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "hashtag",
+                        label = "Select Hashtag",
+                        choices = c("#COVID",
+                                    "#COVID19",
+                                    "#COVID-19",
+                                    "#COVID_19"),
+                        selected = "#COVID",
+                        multiple = FALSE)
+        ),
+
+
+        mainPanel(
+           plotOutput("wordcloud", height = 500, width = 500),
+           tableOutput("sum_table"),
+           plotOutput("sum_bar")
+        )
+    )
+)
+
+# Define server logic
+server <- function(input, output) {
+    # Authentication process
+    create_token(
+        app = "finalproject",
+        consumer_key = "apikeygoeshere",
+        consumer_secret = "apisecretgoeshere",
+        access_token = "tokengoeshere",
+        access_secret = "secrettokengoeshere",
+    )
+    
+    process_tweets <- function(term){
+        # Pull in the tweets
+        tweets <- search_tweets(term,
+                                n = 500,
+                                include_rts = FALSE)
+        
+        # Function to create unigrams from data
+        # only look at unigrams because believe those will be most informative
+        tokenizer <- function(x) {
+            NGramTokenizer(x, Weka_control(min = 1, max = 1))
+        }
+        
+        tweets$text <- tweets$text %>%
+            iconv("UTF-8", "ASCII", sub = "")
+        
+        # Create corpus and preprocess the tweets
+        tweets_cp <- tweets %>%
+            add_column(doc_id = 1:nrow(tweets), .before = 1) %>%
+            DataframeSource() %>%
+            VCorpus() %>%
+            # remove hashtags
+            tm_map(content_transformer(function(x) str_remove_all(x, pattern = "#\\w*"))) %>%
+            # remove urls
+            tm_map(content_transformer(function(x) str_remove_all(x, "https://[\\w|.|/]*"))) %>%
+            tm_map(content_transformer(replace_abbreviation)) %>%
+            tm_map(content_transformer(replace_contraction)) %>%
+            tm_map(removePunctuation) %>%
+            tm_map(removeNumbers) %>%
+            tm_map(content_transformer(str_to_lower)) %>%
+            # Remove english and spanish stop words as both were present in initial work
+            # Also remove covid as it is expected to be in many of the tweets with a covid hashtag
+            tm_map(removeWords,c(stopwords("en"), stopwords("spanish"), "covid")) %>%
+            tm_map(stripWhitespace) %>%
+            tm_map(content_transformer(lemmatize_strings))
+        
+        # Tokenize and remove sparse terms
+        tweets_dtm <- DocumentTermMatrix(tweets_cp,
+                                        control = list(tokenize = tokenizer)) %>%
+                        removeSparseTerms(.99)
+        
+        # convert DTM to a tibble for analysis and remove the search term "dog"
+        tweets_tbl <- as_tibble(as.matrix(tweets_dtm)) 
+        
+        # Remove rows with no remaining terms
+        tweets_tbl <- tweets_tbl[rowSums(tweets_tbl) > 0,]
+        
+        return(tweets_tbl)
+    }
+    
+    covid_tbl <- process_tweets("#COVID")
+    covid19_tbl <- process_tweets("#COVID19")
+    dash_tbl <- process_tweets("#COVID-19")
+    underscore_tbl <- process_tweets("#COVID_19")
+
+    # Create a word cloud based on the hashtag that is selected
+    output$wordcloud <- renderPlot({
+        tbl <- switch(input$hashtag, 
+                        "#COVID" = covid_tbl,
+                        "#COVID19" = covid19_tbl,
+                        "#COVID-19" = dash_tbl,
+                        "#COVID_19" = underscore_tbl)
+        
+        wordcloud(colnames(tbl), 
+                  colSums(tbl), 
+                  max.words = 50, 
+                  colors = "purple")
+    })
+    
+    # Will only be run once because not reactive
+    output$sum_table <- renderTable({
+        # Limit tables to those with word counts > 5
+        covid_5tbl <- colnames(covid_tbl[, colSums(covid_tbl) > 5])
+        covid19_5tbl <- colnames(covid19_tbl[, colSums(covid19_tbl) > 5])
+        dash_5tbl <- colnames(dash_tbl[, colSums(dash_tbl) > 5])
+        underscore_5tbl <- colnames(underscore_tbl[, colSums(underscore_tbl) > 5])
+        
+        table <- data.frame(c(rep("#COVID", 3), rep("#COVID19", 2), "#COVID-19"),
+                            c("#COVID19", "#COVID-19", "#COVID_19", "#COVID-19", "#COVID_19", "#COVID_19"),
+                            c(length(intersect(covid_5tbl, covid19_5tbl)),
+                              length(intersect(covid_5tbl, dash_5tbl)),
+                              length(intersect(covid_5tbl, underscore_5tbl)),
+                              length(intersect(covid19_5tbl, dash_5tbl)),
+                              length(intersect(covid19_5tbl, underscore_5tbl)),
+                              length(intersect(dash_5tbl, underscore_5tbl)))
+                   )
+        colnames(table) <- c("Hashtag 1", "Hashtag 2", "Number of Overlapping Words")
+        table
+    })
+    
+    # Will only be run once because not reactive
+    output$sum_bar <- renderPlot({
+        options(stringsAsFactors = FALSE)
+        covid <- data.frame(word = colnames(covid_tbl), 
+                            count = colSums(covid_tbl))
+        covid19 <- data.frame(word = colnames(covid19_tbl), 
+                              count = colSums(covid19_tbl))
+        dash <- data.frame(word = colnames(dash_tbl), 
+                      count = colSums(dash_tbl))
+        underscore <- data.frame(word = colnames(underscore_tbl), 
+                            count = colSums(underscore_tbl))
+        
+        combined <- full_join(covid, covid19, by = "word") %>%
+                        full_join(dash, by = "word") %>%
+                        full_join(underscore, by = "word") %>%
+                        rowwise() %>%
+                        mutate(total_freq = sum(count.x, count.y, 
+                                                count.x.x, count.y.y, 
+                                                na.rm = TRUE)) %>%
+                        ungroup()
+        
+        top_n(combined, n = 20, wt = total_freq) %>%
+            ggplot( aes(x = reorder(word, total_freq), y = total_freq)) +
+            geom_col(fill = "darkblue") +
+            coord_flip() +
+            labs(x = "Word",
+                 y = "Frequency Across All Hashtags")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ R/projectB.R
@@ -0,0 +1,106 @@
+# R Studio API
+library(rstudioapi)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Libraries
+library(tidyverse)
+library(rvest)
+library(lubridate)
+library(RColorBrewer)
+
+
+# Data Import and Cleaning
+
+# Get the starting page for the search
+page <- read_html("https://scholar.google.com/scholar?start=0&q=%22covid-19%22+source:psychology&hl=en&as_sdt=0,39")
+
+# Find the number of results, so that we know how many pages we need to scrape
+nodes_pages <- html_nodes(page, "#gs_ab_md .gs_ab_mdw")
+num_pages <- as.numeric(str_match(html_text(nodes_pages), "^About ([0-9]*) results")[2])
+
+results <- matrix(NA, nrow = num_pages, ncol = 5)
+colnames(results) <- c("Author", "Journal", "Year", "Title", "Link")
+
+# For each page, scrape info
+for(i in 0:(num_pages%/%10)){
+  # To track progress
+  print(i)
+  
+  # Pull one page at a time
+  page <- read_html(paste0("https://scholar.google.com/scholar?start=",10*i,"&q=%22covid-19%22+source:psychology&hl=en&as_sdt=0,39"))
+  # Find the elements corresponding to article title, author, journal, year, and link
+  nodes_titles <- html_nodes(page, ".gs_rt, h3 span id")
+  nodes_cite <- html_nodes(page, ".gs_a")
+  nodes_link <- html_nodes(page, "h3 a")
+  
+  # Convert html to text and parse for desired information
+  all_info <- html_text(nodes_cite)
+  author <- str_extract(all_info, "([a-zA-Z ,.\\-\\u2026]+)") %>%
+              str_remove_all(" - $")
+  journal <- str_match(all_info, "- ([\\u2026]?[:blank:]?[a-zA-Z ,:&]*[:blank:]?[\\u2026]?)")[,2]%>%
+              str_remove_all(", $| $")
+  year <- str_match(all_info, "-[a-zA-Z ,&\\u2026[:blank:]]* ([0-9]{4})")[,2]
+  # determine whether citation that doesn't have a link
+  has_link <- which(str_detect(html_text(nodes_titles), "\\[CITATION\\]\\[C\\]" ))
+  title <- html_text(nodes_titles) %>%
+              str_remove_all("^(\\[PDF\\]\\[PDF\\] |\\[CITATION\\]\\[C\\] |\\[HTML\\]\\[HTML\\] )") 
+  link <- html_attr(nodes_link,"href")
+  for(j in has_link){
+    link <- append(link, "", after=(j-1))
+  }
+  
+  # Cover situation where link is in title and not in href tag (eg 1st element on first page as of 5/5/2020)
+  for(j in which(str_detect(title, "http"))){
+    ind_link <- str_extract(title[j], "http[a-zA-Z0-9?_\\-/.: ]*")
+    link <- append(link, ind_link, after=(j-1))
+    title[j] <- str_remove(title[j], "http[a-zA-Z0-9?_\\-/.: ]*") %>%
+                  str_remove_all(" $")
+  }
+  
+  # Save results
+  results[(i*10+1):(i*10+length(author)),] <- cbind(author, journal, year, title, link)
+  
+  # To make sure not overdoing request rate
+  Sys.sleep(5)
+}
+
+# Visualization - Plot of number of publications in top 10 journals by Year
+
+# Create tibble from results and make Journal titles uniform by capitalizing
+results <- as_tibble(results) %>%
+              mutate(Journal = str_to_upper(Journal))
+
+# Remove results that don't have a year or a Journal 
+#    because graphing on those elements
+# Count number of results per journal
+arranged_journals <- results %>%
+                       filter(!is.na(Year) & Journal != "") %>%
+                       group_by(Journal)%>%
+                       count() %>%
+                       arrange(desc(n))
+
+# Take the top 10 journals with the most results
+top_10_journals <- arranged_journals[1:10,"Journal"]
+
+# Create a count of results per year and a total count per journal
+#    and make year a date so that can see years that don't have any results
+count_by_journal <- results %>%
+                      filter(Journal %in% pull(top_10_journals)) %>%
+                      select(Journal, Year) %>%
+                      mutate(Year = year(paste0(Year,"-12-12"))) %>%
+                      group_by(Journal, Year) %>%
+                      count() %>% 
+                      group_by(Journal) %>%
+                      mutate(total = sum(n))
+
+# Create the plot
+#   Year along the x-axis and count on the y-axis
+#   Color is done with darkest purple = least popular journal,
+#                   and darkest green = most popular journal
+ggplot(count_by_journal, aes(x = Year, y = n, fill = reorder(Journal, total))) +
+  geom_col(position = "dodge", width = 1) +
+  theme(axis.text.x = element_text(angle = 90)) + 
+  scale_fill_brewer(palette = "PRGn") +
+  labs(y = "Count",
+       fill = "Journal")
+
